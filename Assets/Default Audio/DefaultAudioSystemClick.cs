742c35f1867f7fd6be56f965f21b17c5409491c2
KunalT6569 <kunaltandon.kt@gmail.com>
2018-04-21 19:28:09 +0530

    boilerplate project created

---
+++ Assets/Default Audio/DefaultAudioSystemClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultAudioSystemClick : MonoBehaviour
+{
+    [SerializeField] AudioSource AudioSource;
+    [SerializeField] AudioClip AudioClip;
+
+    public void PlayDefaultAudioSystemClickSound()
+    {
+        AudioSource.PlayOneShot(AudioClip);
+    }
+}
